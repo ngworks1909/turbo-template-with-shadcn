cad83c320b45749cb9d4d2e04a4b1ee2ce4f8377
Nithin1908 <76480955+ngworks1909@users.noreply.github.com>
2024-07-04 22:05:55 +0530

    updated_profile

--- packages/firebase/src/db.ts
+++ packages/firebase/src/db.ts
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import {getStorage} from 'firebase/storage'
 
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB3fpBj_A0pVVzoVyNxLWete3r7p730dYU",
-  authDomain: "tlrs-893dc.firebaseapp.com",
-  projectId: "tlrs-893dc",
-  storageBucket: "tlrs-893dc.appspot.com",
-  messagingSenderId: "164296008201",
-  appId: "1:164296008201:web:66b6fc3139d9912050ced8",
-  measurementId: "G-S9N8273Z0J"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 // Initialize Firebase
