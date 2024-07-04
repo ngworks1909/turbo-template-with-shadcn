5eb75af3279cd4a6c34ebd5dfff759fc9fe27e59
Nithin1908 <76480955+ngworks1909@users.noreply.github.com>
2024-07-04 22:38:49 +0530

    updated_profile

--- packages/firebase/src/db.ts
+++ packages/firebase/src/db.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {getStorage} from 'firebase/storage';
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyB3fpBj_A0pVVzoVyNxLWete3r7p730dYU",
   authDomain: "tlrs-893dc.firebaseapp.com",
