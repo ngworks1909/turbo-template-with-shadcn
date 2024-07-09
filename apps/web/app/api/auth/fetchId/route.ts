7799fe5a700c0fe5af6e95c3d309c79b0207cd7c
Nithin1908 <76480955+ngworks1909@users.noreply.github.com>
2024-07-09 13:31:28 +0530

    added_details

--- apps/web/app/api/auth/fetchId/route.ts
+++ apps/web/app/api/auth/fetchId/route.ts
@@ -2,7 +2,7 @@ import { NEXT_AUTH_CONFIG } from "@repo/ui/auth";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(){
     const session = await getServerSession(NEXT_AUTH_CONFIG);
     const id = session.user.id;
     return NextResponse.json({id});
